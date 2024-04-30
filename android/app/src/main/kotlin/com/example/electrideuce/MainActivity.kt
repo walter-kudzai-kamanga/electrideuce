2db557d39c2ed1f21ed9c7028de1937d5e45b487
Walter kudzai Kamanga <h220052@hit.ac.zw>
2024-04-30 23:27:27 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/electrideuce/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.electrideuce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+
+void main() => runApp(home_screen());
+
+class home_screen extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<home_screen> {
+  int index = 0;
+  final screens = [];
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: screens[index],
+        bottomNavigationBar: NavigationBarTheme(
+          data: NavigationBarThemeData(
+            indicatorColor: Colors.blue.shade100,
+            labelTextStyle: MaterialStateProperty.all(
+              const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+            ),
+          ),
+          child: NavigationBar(
+              height: 60,
+              backgroundColor: Colors.grey.shade300,
+              labelBehavior:
+                  NavigationDestinationLabelBehavior.onlyShowSelected,
+              selectedIndex: index,
+              animationDuration: const Duration(seconds: 3),
+              onDestinationSelected: (index) =>
+                  setState(() => this.index = index),
+              destinations: const [
+                NavigationDestination(
+                  icon: Icon(Ionicons.home_outline),
+                  selectedIcon: Icon(Icons.home),
+                  label: 'Home',
+                ),
+                NavigationDestination(
+                  icon: Icon(Ionicons.reader_outline),
+                  selectedIcon: Icon(Ionicons.reader),
+                  label: 'Indexes',
+                ),
+                NavigationDestination(
+                  icon: Icon(Ionicons.wallet_outline),
+                  selectedIcon: Icon(Ionicons.wallet),
+                  label: 'Payment',
+                ),
+                NavigationDestination(
+                  icon: Icon(Ionicons.list_outline),
+                  selectedIcon: Icon(Ionicons.list),
+                  label: 'History',
+                ),
+                NavigationDestination(
+                  icon: Icon(Ionicons.information_circle_outline),
+                  selectedIcon: Icon(Ionicons.information_circle),
+                  label: 'Infor',
+                ),
+              ]),
+        ),
+      );
+}
